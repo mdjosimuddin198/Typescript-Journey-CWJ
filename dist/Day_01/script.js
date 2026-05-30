85536be21096c50c1187d8d091eef7255677b0e0
josimuddin198 <josimweb0504@gmail.com>
2026-05-30 22:07:32 +0600

    setup TypeScript project structure with src and dist

--- dist/Day_01/script.js
+++ dist/Day_01/script.js
@@ -1,29 +1,30 @@
-"use strict";
 // first code
 console.log("hello world/Ai");
 // declare  variables
 const fname = "md Josim uddin";
 const age = 21;
 const isStudent = true;
-console.log(`Assalamu-Alaikum / Hello Everyone, My Name is ${fname} . I am ${age} years old and I am Currently a  ${isStudent ? "student " : "kamla"} `);
+console.log(`Assalamu-Alaikum / Hello Everyone, My Name is ${fname} . I am ${age} years old and I am Currently a  ${isStudent ? "student " : "not a student"} `);
 // declare array
-const friends = ["Mobasser", "Emran ", "Belal ", "Samirul", "Asadul"];
+const friends = ["Mobasser", "Emran", "Belal", "Samirul", "Asadul"];
 // create  loop
 for (let friend of friends) {
     console.log(friend);
 }
-const student = {
+const studentInfo = {
     name: "md josim uddin",
     age: 21,
     isStudent: true,
     isHireable: () => {
-        console.log("no perfect yet");
+        console.log("not perfect yet");
     },
 };
-console.log(student);
+console.log(studentInfo);
 // make  function
 const add = (a, b) => {
     return a + b;
 };
-const result = add(5.6, 8);
+const result = add(5, 8);
 console.log(result);
+export {};
+//# sourceMappingURL=script.js.map
